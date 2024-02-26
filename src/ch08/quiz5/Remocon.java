ee46915407df6b323c4667ea294dc47be4101ac3
parkjeongmin1 <gars150@naver.com>
2024-02-26 17:53:21 +0900

    up

---
+++ src/ch08/quiz5/Remocon.java
@@ -0,0 +1,5 @@
+package ch08.quiz5;
+
+public interface Remocon {
+    public void powerOn();
+}
---
+++ src/ch08/quiz6/Cat.java
@@ -0,0 +1,8 @@
+package ch08.quiz6;
+
+public class Cat implements Soundable{
+    @Override
+    public String sound() {
+       return "야옹";
+    }
+}
